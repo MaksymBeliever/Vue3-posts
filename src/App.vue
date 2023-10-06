e01107dc86eb2d847020a93ee6b9800a4f98f4b9
max <max.beliba@alternatestates.com>
2023-10-06 23:49:38 +0300

    Created the base structure of the project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div class="post" v-for="post of posts" :key="post.id">
+      <div><strong>Name:</strong> {{ post.title }}</div>
+      <div><strong>Description:</strong> {{ post.body }}</div>
+    </div>
+  </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        posts: [
+          { id: 1, title: 'JavaScript', body: 'description' },
+          { id: 2, title: 'TypeScript', body: 'description' },
+          { id: 3, title: 'Vue', body: 'description' },
+          { id: 4, title: 'React', body: 'description' },
+        ],
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .post {
+    margin-top: 15px;
+    padding: 15px;
+    border: 2px solid teal;
+  }
 </style>
